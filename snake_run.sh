40167a19540ab946053e1a736428c61c235b2d32
michal7kw <michal.k7w@gmail.com>
2024-10-23 15:29:27 +0200

    sync

--- snake_run.sh
+++ snake_run.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
-#SBATCH --job-name=chipseq
+#SBATCH --job-name=Bisulf
 #SBATCH --account=kubacki.michal
 #SBATCH --mem=128GB
-#SBATCH --time=INFINITE
-#SBATCH --nodes=2
-#SBATCH --ntasks=32
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
 #SBATCH --mail-type=ALL
 #SBATCH --exclusive
 #SBATCH --mail-user=kubacki.michal@hsr.it
-#SBATCH --error="/beegfs/scratch/ric.broccoli/kubacki.michal/SRF_Bisulf/logs/log.err"
-#SBATCH --output="/beegfs/scratch/ric.broccoli/kubacki.michal/SRF_Bisulf/logs/log.out"
+#SBATCH --error="/beegfs/scratch/ric.broccoli/kubacki.michal/SRF_Bisulf/logs/bisulf.err"
+#SBATCH --output="/beegfs/scratch/ric.broccoli/kubacki.michal/SRF_Bisulf/logs/bisulf.out"
 
-# Load the appropriate conda environment
-source /opt/common/tools/ric.cosr/miniconda3/bin/activate
+# # Load the appropriate conda environment
+# source /opt/common/tools/ric.cosr/miniconda3/bin/activate
 
-# Create and activate the environment
-conda env create -f environment.yaml
-conda activate bisulfite_seq
+# # Create and activate the environment
+# conda env create -f environment.yaml
+# conda activate bisulfite_seq
 
-# Run Snakemake for all samples
-# snakemake --unlock
-# snakemake \
-#     --snakefile Snakefile \
-#     --latency-wait 60 \
-#     --configfile config.yaml \
-#     -p \
-#     --printshellcmds \
-#     --keep-going \
-#     --verbose \
-#     --rerun-incomplete \
-#     --cores all
+# Load the conda environment
+source /opt/common/tools/ric.cosr/miniconda3/bin/activate bisulfite_seq
 
-# Run Snakemake for a single sample (e.g., NEU1)
+# Check if the genome file exists
+GENOME_FILE="/beegfs/scratch/ric.broccoli/kubacki.michal/SRF_Bisulf/DATA/genome.fa"
+if [ ! -f "$GENOME_FILE" ]; then
+    echo "Error: Genome file not found at $GENOME_FILE"
+    exit 1
+fi
+
+# Unlock the working directory
 snakemake --unlock
+
+# Run Snakemake
 snakemake \
-    --snakefile Snakefile \
+    --snakefile Snakefile_mod \
     --latency-wait 60 \
     --configfile config.yaml \
     --config debug_sample=NEU1 \
@@ -42,20 +41,13 @@ snakemake \
     --printshellcmds \
     --verbose \
     --rerun-incomplete \
-    --cores all
+    --use-conda \
+    --cores all \
+    --jobs 100 \
+    --max-jobs-per-second 1 \
+    --keep-going \
+    --restart-times 3
 
-#    --keep-going \
-
-# Dry run Snakemake for a single sample (e.g., NEU1)
-# snakemake \
-#     --snakefile Snakefile \
-#     --configfile config.yaml \
-#     --config debug_sample=NEU1 \
-#     -np \
-#     --rerun-incomplete \
-#     --cores all
-
-# snakemake --unlock
 # snakemake \
 #     --snakefile Snakefile \
 #     --latency-wait 60 \
@@ -63,23 +55,9 @@ snakemake \
 #     --config debug_sample=NEU1 \
 #     -p \
 #     --printshellcmds \
-#     --keep-going \
 #     --verbose \
 #     --rerun-incomplete \
 #     --use-conda \
-#     --cores all
+#     --cores 32
+    # --cluster "sbatch --parsable --job-name=Bisulf_{rule} --mem={resources.mem_mb} --time={resources.time} --cpus-per-task={threads}"
 
-# snakemake --unlock
-# snakemake \
-#     --snakefile Snakefile \
-#     --latency-wait 60 \
-#     --configfile config.yaml \
-#     --config debug_sample=NEU1 \
-#     -p \
-#     --printshellcmds \
-#     --keep-going \
-#     --verbose \
-#     --rerun-incomplete \
-#     --use-conda \
-#     --cores all \
-#     --until multiqc
